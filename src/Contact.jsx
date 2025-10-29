cff36371e2bef5abc3fad5faeec72872507d69e6
Iyanu <adisaiyanuakorede@gmail.com>
2025-10-29 10:46:17 +0100

    corrcetions

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,5 @@
 import { useRef, useState } from "react";
 import { sendForm } from "@emailjs/browser";
-import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 
 export default function Contact() {
   const form = useRef();
